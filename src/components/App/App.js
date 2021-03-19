60781063bd23326952713e0f4ddacf18adf56479
Chakrygin Maxim <m.chakrygin@gmail.com>
2021-03-19 15:18:01 +0300

    feat: добавить роуты

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+import Main from "../Main";
+import Movies from "../Movies";
+import SavedMovies from "../SavedMovies";
+import Profile from "../Profile";
+import Register from "../Register";
+import Login from "../Login";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <Route path='/'>
+          <Main />
+        </Route>
+        <Route path='/movies'>
+          <Movies />
+        </Route>
+        <Route path='/saved-movies'>
+          <SavedMovies />
+        </Route>
+        <Route path='/profile'>
+          <Profile />
+        </Route>
+        <Route path='/signup'>
+          <Register />
+        </Route>
+        <Route path='/signin'>
+          <Login />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
