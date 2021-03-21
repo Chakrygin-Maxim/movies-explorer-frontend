d0fc5e4e24f7bcb520d32d64119e681ae1eac94c
Chakrygin Maxim <m.chakrygin@gmail.com>
2021-03-21 13:56:00 +0300

    feat: добавить блок aboutme

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,60 @@
+import "./AboutMe.css";
+import avatar from "../../images/avatar.jpg";
+
+function AboutMe(props) {
+  return (
+    <section className='aboutMe'>
+      <h2 className='aboutMe__header'>Студент</h2>
+      <div className='aboutMe__container'>
+        <div className='aboutMe__leftColumn'>
+          <div className='aboutMe__topCell'>
+            <h3 className='aboutMe__title'>Максим</h3>
+            <p className='aboutMe__subtitle'>Фронтенд-разработчик, 29 лет</p>
+            <p className='aboutMe__text'>
+              Я родился и живу в Москве, закончил факультет экономики РГТЭУ.
+              Увлекаюсь парусным спортом и сноубордом. С 2012 года занимаюсь
+              разработкой на языке 1С. С 2016 года работаю в Яндексе
+              программистом 1С. Со временем понял что необходимо развивать
+              другие навыки и начал самостоятельно изучать JavaScript и верстку.
+              В скором времени поступил на курс веб-разработки Яндекс.Практикума
+              и уже применяю полученыне знания в своей работе.
+            </p>
+          </div>
+          <div className='aboutMe__bottomCell'>
+            <ul className='aboutMe__socialList'>
+              <li className='aboutMe__social-item'>
+                <a
+                  className='aboutMe__social-link'
+                  href='https://www.facebook.com/maks.chakrygin/'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  Facebook
+                </a>
+              </li>
+              <li className='aboutMe__social-item'>
+                <a
+                  className='aboutMe__social-link'
+                  href='https://github.com/Chakrygin-Maxim'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  GitHub
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='aboutMe__rightColumn'>
+          <img
+            src={avatar}
+            alt='Аватарка Максима'
+            className='aboutMe__avatar'
+          ></img>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default AboutMe;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,6 +2,7 @@ import "./Main.css";
 import Promo from "../Promo/Promo";
 import AboutProject from "../AboutProject/AboutProject";
 import Techs from "../Techs/Techs";
+import AboutMe from "../AboutMe/AboutMe";
 
 function Main(props) {
   return (
@@ -9,6 +10,7 @@ function Main(props) {
       <Promo />
       <AboutProject />
       <Techs />
+      <AboutMe />
     </main>
   );
 }
